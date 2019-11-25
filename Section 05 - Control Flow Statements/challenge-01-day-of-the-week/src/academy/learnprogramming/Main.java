b55faac003dac8d0adba295aeaba3fba20118689
amb690 <albertombln@gmail.com>
2019-11-25 20:29:30 +0100

    Adds switch example and challenge.

---
+++ Section 05 - Control Flow Statements/challenge-01-day-of-the-week/src/academy/learnprogramming/Main.java
@@ -0,0 +1,66 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
+    }
+
+    private static void printDayOfTheWeek(int day) {
+        switch(day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+
+    // Another solution with if ... else
+//    private static void printDayOfTheWeek(int day) {
+//        if (day == 0) {
+//            System.out.println("Sunday");
+//        } else if (day == 1) {
+//            System.out.println("Monday");
+//        } else if (day == 2) {
+//            System.out.println("Tuesday");
+//        } else if (day == 3) {
+//            System.out.println("Wednesday");
+//        } else if (day == 4) {
+//            System.out.println("Thursday");
+//        } else if (day == 5) {
+//            System.out.println("Friday");
+//        } else if (day == 6) {
+//            System.out.println("Saturday");
+//        } else {
+//            System.out.println("Invalid day");
+//        }
+//    }
+}
---
+++ Section 05 - Control Flow Statements/example-01-switch/src/academy/learnprogramming/Main.java
@@ -0,0 +1,70 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int value = 3;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3, or a 4, or a 5");
+                System.out.println("Actually is was a " + switchValue);
+//                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+        // Create a new switch statements using char instead of int
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+
+        char value = 'E';
+        switch(value) {
+            case 'A':
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was B");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println(value + " was found");
+                break;
+            default:
+                System.out.println("Could not find A, B, C, D or E");
+                break;
+        }
+
+        String month = "June";
+        switch(month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+    }
+}
