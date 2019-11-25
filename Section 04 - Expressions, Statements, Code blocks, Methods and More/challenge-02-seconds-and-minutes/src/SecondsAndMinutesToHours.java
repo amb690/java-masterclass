84a0cc40698af40d87795c0b06b9a3d8395a5b98
amb690 <albertombln@gmail.com>
2019-11-25 15:42:45 +0100

    Seconds and minutes to hours challenge: appends leading 0 to less than ten seconds, minutes and hours output strings.

--- Section 04 - Expressions, Statements, Code blocks, Methods and More/challenge-02-seconds-and-minutes/src/SecondsAndMinutesToHours.java
+++ Section 04 - Expressions, Statements, Code blocks, Methods and More/challenge-02-seconds-and-minutes/src/SecondsAndMinutesToHours.java
@@ -1,24 +1,27 @@
 public class SecondsAndMinutesToHours {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            return "Invalid Value";
-        }
-
-        if (minutes == 61 && seconds == 0) {
-            return "01h 01m 00s";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int totalSeconds = minutes * 60 + seconds;
         int hours = totalSeconds / 3600;
         int remainingMinutes = (totalSeconds % 3600) / 60;
         int remainingSeconds = totalSeconds % 60;
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+
+        String hoursString = (hours < 10? "0" + hours: hours) + "h ";
+        String minutesString = (remainingMinutes < 10? "0" + remainingMinutes: remainingMinutes) + "m ";
+        String secondsString = (remainingSeconds < 10? "0" + remainingSeconds: remainingSeconds) + "s";
+
+        return hoursString + minutesString + secondsString;
     }
 
     public static String getDurationString(int seconds) {
         if (seconds < 0) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
