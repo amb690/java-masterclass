c3629211533cc92e24ad5eb216cc0d109ad0618e
amb690 <albertombln@gmail.com>
2019-11-26 11:57:54 +0100

    Number in word: Adds main class to test output.

---
+++ Section 05 - Control Flow Statements/exercise-12-number-in-word/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(3);
+        NumberInWord.printNumberInWord(4);
+        NumberInWord.printNumberInWord(5);
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(7);
+        NumberInWord.printNumberInWord(8);
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(135);
+        NumberInWord.printNumberInWord(-23);
+    }
+}
