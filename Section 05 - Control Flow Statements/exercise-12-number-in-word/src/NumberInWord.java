a77953866eae26e07e3446440a12d79027d96915
amb690 <albertombln@gmail.com>
2019-11-26 10:42:54 +0100

    Adds number in word exercise.

---
+++ Section 05 - Control Flow Statements/exercise-12-number-in-word/src/NumberInWord.java
@@ -0,0 +1,40 @@
+public class NumberInWord {
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+}
