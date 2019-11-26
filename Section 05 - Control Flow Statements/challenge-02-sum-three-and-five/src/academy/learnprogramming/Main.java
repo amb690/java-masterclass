ae5938c94e904dcbcc94378c303b5c898c5574ec
amb690 <albertombln@gmail.com>
2019-11-26 15:38:10 +0100

    Another solution for the sum 3 and 5 challenge.

--- Section 05 - Control Flow Statements/challenge-02-sum-three-and-five/src/academy/learnprogramming/Main.java
+++ Section 05 - Control Flow Statements/challenge-02-sum-three-and-five/src/academy/learnprogramming/Main.java
@@ -6,15 +6,24 @@ public class Main {
 
         int total = 0;
         int count = 0;
-	    for (int i=1; i<1001; i++) {
-	        if (i%15 == 0) {
-                System.out.println("Found number " + i);
-	            total += i;
-	            count++;
-                if (count == 5) {
-                    break;
-                }
-	        }
+//	    for (int i=1; i<1001; i++) {
+//	        if (i%15 == 0) {
+//                System.out.println("Found number " + i);
+//	            total += i;
+//	            count++;
+//                if (count == 5) {
+//                    break;
+//                }
+//	        }
+//        }
+
+        for (int i = 15; i < 1001; i=i+15) {
+            System.out.println("Found number " + i);
+            total += i;
+            count++;
+            if (count == 5) {
+                break;
+            }
         }
         System.out.println("Sum of the numbers divisible by 3 and 5 = " + total);
     }
