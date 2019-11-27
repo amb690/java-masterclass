daac98ebf9ed34ac09bb40d63ff09e4577845ae6
amb690 <albertombln@gmail.com>
2019-11-27 11:38:06 +0100

    Adds first and last digit sum exercise.

---
+++ Section 05 - Control Flow Statements/exercise-16-sum-first-and-last-digit/src/FirstLastDigitSum.java
@@ -0,0 +1,14 @@
+public class FirstLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+        while (number > 9) {
+            number /= 10;
+        }
+        return lastDigit + number;
+    }
+}
---
+++ Section 05 - Control Flow Statements/exercise-16-sum-first-and-last-digit/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sum first and last digit of number 252 is " + FirstLastDigitSum.sumFirstAndLastDigit(252));
+        System.out.println("Sum first and last digit of number 257 is " + FirstLastDigitSum.sumFirstAndLastDigit(257));
+        System.out.println("Sum first and last digit of number 0 is " + FirstLastDigitSum.sumFirstAndLastDigit(0));
+        System.out.println("Sum first and last digit of number 5 is " + FirstLastDigitSum.sumFirstAndLastDigit(5));        System.out.println("Sum first and last digit of number 252 is " + FirstLastDigitSum.sumFirstAndLastDigit(252));
+        System.out.println("Sum first and last digit of number -10 is " + FirstLastDigitSum.sumFirstAndLastDigit(-10));
+        System.out.println("Sum first and last digit of number 10 is " + FirstLastDigitSum.sumFirstAndLastDigit(10));
+
+    }
+}
