0438f0856360de740d8829d514cda2dee51eefee
Alberto Morante Bailón <albertombln@gmail.com>
2019-12-07 18:11:04 +0100

    Avoid non integer inputs. Adds age validation

--- Section 05 - Control Flow Statements/example-05-reading-user-input/src/academy/learnprogramming/Main.java
+++ Section 05 - Control Flow Statements/example-05-reading-user-input/src/academy/learnprogramming/Main.java
@@ -8,14 +8,25 @@ public class Main {
 	    Scanner scanner = new Scanner(System.in);
 
 	    System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2019 - yearOfBirth;
+	    boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2019 - yearOfBirth;
+
+            if (age >= 0 && age <= 1000) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
 
         scanner.close();
     }
