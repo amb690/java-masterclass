400086259ad5123699c33dc93ebc9049da81129d
Alberto Morante Bailón <albertombln@gmail.com>
2019-12-09 12:37:49 +0100

    Adds class example.

---
+++ Section 06 - OOP Part 1 - Classes, Constructors and Inheritance/example-01-classes/src/academy/learnprogramming/Car.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ Section 06 - OOP Part 1 - Classes, Constructors and Inheritance/example-01-classes/src/academy/learnprogramming/Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car();
+	    Car holden = new Car();
+	    porsche.setModel("911");
+		System.out.println("Model is " + porsche.getModel());
+    }
+}
