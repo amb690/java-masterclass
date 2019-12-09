8ef526d052a8a18da3f9e846e183274ca154d260
Alberto Morante Bailón <albertombln@gmail.com>
2019-12-09 11:28:22 +0100

    Paint Job exercise: shorten some return sentences.

--- Section 05 - Control Flow Statements/exercise-28-paint-job/src/PaintJob.java
+++ Section 05 - Control Flow Statements/exercise-28-paint-job/src/PaintJob.java
@@ -9,10 +9,18 @@ public class PaintJob {
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
-        return width <= 0.0 || height <= 0.0 || areaPerBucket <= 0.0? -1: getBucketCount(width * height, areaPerBucket);
+        if (width <= 0.0 || height <= 0.0 || areaPerBucket <= 0.0) {
+            return -1;
+        }
+
+        return getBucketCount(width * height, areaPerBucket);
     }
 
     public static int getBucketCount(double area, double areaPerBucket) {
-        return area <= 0.0 || areaPerBucket <= 0.0? -1: (int) Math.ceil(area/areaPerBucket);
+        if ( area <= 0.0 || areaPerBucket <= 0.0) {
+            return -1;
+        }
+
+        return (int) Math.ceil(area/areaPerBucket);
     }
 }
